3d6dac5c8459f529935f106fefbefb865e56568f
Ddlysa <ddlysa2@gmail.com>
2025-07-15 00:20:07 +0700

    Add files via upload

---
+++ coba/app/Models/SensorReading.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SensorReading extends BaseModel
+{
+    
+    protected $table = 'sensor_readings';
+    protected $fillable = ['temperature', 'ppm', 'water_level', 'created_at'];
+
+
+}
